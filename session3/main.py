0278b0adfb4ed4926983007e3165a529771c0759
amirhajif <amirhajitabar2@gmail.com>
2022-07-06 03:30:51 +0430

    session3 Files

---
+++ session3/main.py
@@ -0,0 +1,42 @@
+#Conditions
+#agar --> if darghey insorat-->else
+
+number1=20
+number2=20
+
+# if number1>=number2:
+#     print("number1 is bigger")
+# else:
+#     print("number2 is bigger")
+
+# if number1 == number2:
+#     print("equal")
+# else:
+#     print("not equal")
+
+# if number1 != number2:
+#     print("not equal")
+# else:
+#     print("equal")
+
+# if number1%2 == 0:
+#     print("zoj")
+# else:
+#     print("fard")
+
+#elif
+day=3
+if day==1:
+    print("shanbeh")
+elif day==2:
+    print("1shanbeh")
+elif day==3:
+    print("2shanbeh")
+elif day==4:
+    print("3shanbeh")
+elif day==5:
+    print("4shanbeh")
+elif day==6:
+    print("5shanbeh")
+else:
+    print("invalid")
