d9fd7b79aa2495c5912ddf776dbffb24a7943214
amirhajif <amirhajitabar2@gmail.com>
2022-07-06 03:13:09 +0430

    session2 Files

---
+++ session2/main.py
@@ -0,0 +1,14 @@
+# x=2
+# y=6
+# x*=y
+# print(x**y)
+# /-->kharej ghesmat %-->baghi mandeh ro bar migradanad
+
+
+#input
+# name=input("Enter Your Name: ")
+
+# print("Your Name is: "+name)
+
+number=int(input())
+print(number**2)
