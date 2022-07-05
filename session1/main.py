4c4b387f14f992487eb58f3b0698d61a6fb1380d
amirhajif <amirhajitabar2@gmail.com>
2022-07-06 02:54:33 +0430

    session1 File

---
+++ session1/main.py
@@ -0,0 +1,20 @@
+#hello to python crash course
+# x=5 #int
+
+# print(type(x))
+# name = "amirhossein"    #str
+# isFind = False  #bool
+# f=12.5  #float
+
+# x="hello"
+
+# print(type(x))
+
+
+#1- be hich vajh ba adad namgozari nakonim
+#2-Case sensetive 3- alaem ajib gharib 
+# y=6
+# Y=6
+
+Text="HELLO"
+print(Text.lower())
