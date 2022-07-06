5b371e212332c7af61b91ed51dad1d8a4bd68e15
amirhajif <amirhajitabar2@gmail.com>
2022-07-06 15:25:34 +0430

    session5 files

---
+++ session5/main.py
@@ -0,0 +1,16 @@
+#loop
+# i=1
+# while i<=20:
+#     print(i)
+#     i+=1
+
+#break & continue
+# i=0
+# while i<=8:
+#     i+=1
+#     if i==4: 
+#         continue
+#     print(i)
+
+for i in range(2,8,2) :
+    print(i)
