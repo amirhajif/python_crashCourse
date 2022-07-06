2abd98cff8cc92666bdf6b59e34af8d6e78cea05
amirhajif <amirhajitabar2@gmail.com>
2022-07-06 14:39:03 +0430

    session 4 files

---
+++ session4/main.py
@@ -0,0 +1,34 @@
+#short hand if else
+number1=10
+number2=20
+
+# if number1>number2:
+#     print("1")
+# else:
+#     print("2")
+
+#print("1") if number1>number2 else print("2")
+
+# IsUserNameCorrect=False
+# IsPasswordCorrect=False
+
+# if IsPasswordCorrect==True or IsUserNameCorrect==True:
+#     print("Login")
+# else:
+#     print("cant login")
+
+# number3=30
+
+# if number3>20:
+#     print("bigger than 20")
+#     if number3>40:
+#         print("bigger 40")
+#     else:
+#         print("not bigger than 40")
+# else:
+#     print("not bigger than 20")
+
+if number1>number2:
+    print("1")
+else:
+    pass
