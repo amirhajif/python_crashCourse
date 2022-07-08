0652b08edd2b8d6cca0fc110814dc1a05a77f8b8
amirhajif <amirhajitabar2@gmail.com>
2022-07-08 13:05:59 +0430

    session6 files

---
+++ session6/main.py
@@ -0,0 +1,24 @@
+classStudent=["ali","reza","amir"]
+classStudent2=list(["amir","ali","ahmad"])
+# print(classStudent)
+# print(classStudent2)
+
+# print(classStudent[0:2])
+# classStudent.append("mohammad")
+# classStudent.insert(1, "ahmad")
+# classStudent[0]="amirali"
+
+# testList=["test",12,True]
+
+# classStudent.remove("amir")
+# classStudent.pop(1)
+# print(classStudent)
+
+# for name in classStudent:
+#     print(name)
+
+# for i in range(len(classStudent)):
+#     print(classStudent[i])
+
+classStudent.sort()
+print(classStudent)
